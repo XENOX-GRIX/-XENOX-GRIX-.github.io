f6a4c95e20ad171acbd5f01b16f827d929464fcb
XENOX-GRIX <xenoxgrik@gmail.com>
2021-06-14 20:56:59 +0530

    Added a background sound to home page

--- jav.js
+++ jav.js
@@ -10,4 +10,19 @@ window.addEventListener('scroll', function(){
 	mountain.style.top=val*0.4 +'px';
 	road.style.top=val*0.15 +'px';
 	txt.style.top=val*0.8+'px';
-})
+});
+var audio = new Audio( 'sounds/bg-music.mp3' );
+audio.play();
+window.setTimeout( function ( ) {
+	audio.volume = 0.06;
+},0);
+
+
+// let music = new Audio({
+//     loop: true,
+//     src: []
+// });
+// music.volume=0.000001;
+// music.play(); 
+
+// alert(music.volume);
